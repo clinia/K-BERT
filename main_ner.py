43da26631d8e6a8f9a31257dd1713b140def5543
AlexandreDuperre <alexandre.duperre@clinia.com>
2021-07-21 15:48:32 -0400

    feat: added clinia kg

--- main_ner.py
+++ main_ner.py
@@ -9,10 +9,12 @@ class NERKBERTArgs:
 
         CHINESE MODEL : 0
 
-        ENGLISH MODEL : 1
+        ENGLISH MODEL WITH TRANSLATED CHINESE KG: 1
+
+        ENGLISH MODEL WITH CLINIA KG: 1
 
         """
-        model = 1
+        model = 2
 
         if model == 0:
             self.pretrained_model_path = "./models/pytorch_model.bin"
@@ -40,6 +42,22 @@ class NERKBERTArgs:
             self.batch_size = 128
             self.kg_name = "brain/kgs/medical_kg_en.spo"
             self.output_model_path = "./outputs/kbert_medical_ner_Medical_en.bin"
+            self.labels_path = "datasets/clinia_ner/ser_bus/labels.csv"
+            self.seq_length = 50
+
+        if model == 2:
+            self.pretrained_model_path = "./models/en/bert_base_uncased/pytorch_model.bin"
+            self.config_path = "./models/en/bert_base_uncased/config.json"
+            self.vocab_path = "./models/en/bert_base_uncased/vocab.txt"
+            self.tokenizer_config = "./models/en/bert_base_uncased/tokeniser_config.json"
+            self.tokenizer = ".model/en/bert_base_uncased/tokenizer.json"
+            self.train_path = "./datasets/clinia_ner/ser_bus/validation/data.csv"  ###*** to change to train
+            self.dev_path = "./datasets/clinia_ner/ser_bus/validation/data.csv"
+            self.test_path = "./datasets/clinia_ner/ser_bus/test/data.csv"
+            self.epochs_num = 5
+            self.batch_size = 128
+            self.kg_name = "brain/kgs/clinia_kg.spo"
+            self.output_model_path = "./outputs/kbert_medical_ner_clinia_kg_en.bin"
             self.labels_path = "datasets/clinia_ner/ser_bus/labels.csv"
             self.seq_length = 50
 
